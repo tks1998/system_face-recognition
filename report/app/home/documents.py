988593b952b6ea061c258e5a560467ad4e568d7d
Quan <quantran.quou.6666@gmail.com>
2019-12-31 20:51:42 +0700

    fix

--- report/app/home/documents.py
+++ report/app/home/documents.py
@@ -1,42 +1,44 @@
-# from django_elasticsearch_dsl import Document
-# from django_elasticsearch_dsl.registries import registry
-# from .models import Information_Face
-# from elasticsearch_dsl import connections
-# from datetime import datetime
-# connections.configure(
-#     default={'hosts': 'localhost'},
-#     dev={
-#         'hosts': ['http://localhost:9200'],
-#         'sniff_on_start': True
-#     }
-# )
+from django_elasticsearch_dsl import Document
+from django_elasticsearch_dsl.registries import registry
+from .models import Information_Face
+from elasticsearch_dsl import connections
+from datetime import datetime
+connections.configure(
+    default={'hosts': 'localhost'},
+    dev={
+        'hosts': ['http://localhost:9200'],
+        'sniff_on_start': True
+    }
+)
 
-# @registry.register_document
-# class Information(Document):
-#     class Index:
-#         name = 'face_system'
 
-#         settings = {'number_of_shards': 1,
-#                     'number_of_replicas': 0}
+@registry.register_document
+class Information(Document):
+    class Index:
+        name = 'face_system'
 
-#     class Django:
-#         model = Information_Face 
-#         fields = [
-#             'name',
-#             'description',
-#             'time',
-#         ]
+        settings = {'number_of_shards': 1,
+                    'number_of_replicas': 0}
 
-# name1 = Information_Face(
-#     name="2",
-#     description="No Nanme T_T",
-#     time =  datetime.now(),
-# )
-# name1.save()
+    class Django:
+        model = Information_Face
+        fields = [
+            'name',
+            'description',
+            'time',
+        ]
 
-# name1 = Information_Face(
-#     name="5",
-#     description="No Nanme T_T",
-#     time= datetime.now(),
-# )
-# name1.save()
+
+name1 = Information_Face(
+    name="2",
+    description="No Nanme T_T",
+    time=datetime.now(),
+)
+name1.save()
+
+name1 = Information_Face(
+    name="5",
+    description="No Nanme T_T",
+    time=datetime.now(),
+)
+name1.save()
--- report/app/home/models.py
+++ report/app/home/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
+
 class Information_Face(models.Model):
-    name = models.CharField(max_length= 100)
-    description = models.TextField(max_length = 100)
-    time = models.DateTimeField() 
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=100)
+    time = models.DateTimeField()
